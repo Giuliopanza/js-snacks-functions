cc6b2c999ed3de0b916c74170ebf22e91fee725c
Giulio <giuliopanza2000@gmail.com>
2025-01-16 18:43:12 +0100

    fine vocali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,11 +2,33 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
+const lettera = "A";
+
 
 // Dichiara la funzione qui.
 
+function Filtra(a,x){
+
+    let b =[]
+
+    for (let i = 0; i < a.length; i++) {
+
+        if (a[i].charAt(0) == x) {
+
+            b.push(a[i])
+            
+        }
+        
+    }
+
+    console.log(b);
+
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
+
+Filtra(names, lettera)
 
 
 
--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,33 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function vocali (a){
+
+    let s = a.split(``);
+
+    let voc = [`a`, `e`, `i`, `o`, `u`] ;
+
+    let b = [] ;
+
+    for (let i = 0; i < s.length; i++) {
+        for (let x = 0; x < voc.length; x++) {
+            if (s[i] == voc[x]) {
+
+                b.push(s[i])
+                
+            }
+        }
+        
+    }
+
+    console.log(b.length  + b)
+
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+vocali(word)
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
